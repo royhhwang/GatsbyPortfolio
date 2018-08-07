b81abcef4862d1bca7994a639c3cdda1be30b56c
Roy Hwang <royhhwang@gmail.com>
2018-08-07 16:27:46 -0700

    page trans

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
+import Header from './header.js'
 import Banner from './banner.js'
 import Work from './work.js'
 import About from './about.js'
 import Contact from './contact.js'
-import Header from './header.js'
+import Transition from './page-2.js'
 
 const IndexPage = () => (
   <div>
@@ -13,6 +14,7 @@ const IndexPage = () => (
     <Work />
     <About />
     <Contact />
+    <Transition />
   </div>
 )
 
---
+++ src/pages/page-2.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import PageTransition from 'react-router-page-transition'
+
+const Transition = () => (
+    <div>
+        <h1>helo world</h1>
+    </div>
+)
+
+export default Transition
