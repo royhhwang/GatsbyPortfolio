0f0f7682ff391d7812a129045e6a7309b6966352
Chloe Hwang <c.chloeh@gmail.com>
2018-08-11 11:05:03 -0400

    add plugin to compile sass and watch for changes

--- src/components/banner.js
+++ src/components/banner.js
@@ -7,18 +7,18 @@ import '../css/banner.css'
 
 const Banner = () => (
     <div className="banner-layer" id="#top">
-        <Fade top>
+        {/* <Fade top>
             <div className="banner-block title-block">
                 <img src={Title} alt="roy title" id="left-face" />
             </div>
-        </Fade>
-        <Fade right>
+        </Fade> */}
+        {/* <Fade right> */}
             <div className="banner-block face-block">
                 <a href="#about">
                     <img src={Face} alt="roy face" id="right-face" />
                 </a>
             </div>
-        </Fade>
+        {/* </Fade> */}
     </div>
 )
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,35 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import { Flip } from 'react-reveal';
-import "../css/header.css";
+import "../scss/header.scss";
 
 const Header = () => (
-    <div
-        style={{
-            background: 'transparent',
-            marginBottom: '1.45rem',
-        }}
-    >
-        <div className="header-layer">
-            <Flip bottom>
-                <h1 className="header-block">
-                    <Link className="header-link"
-                        to="/#contact"
-                    >
-                        Contact
-                    </Link>
-                    <Link className="header-link"
-                        to="/#about"
-                    >
-                        About
-                    </Link>
-                    <Link className="header-link"
-                        to="/#projects"
-                    >
-                        Projects
-                    </Link>
-                    <Link className="header-link"
-                        to="/"
-                    >
-                        Home
-                    </Link>
-                </h1>
-            </Flip>
-        </div>
-    </div >
+    <div className="nav-layer">
+        {/* <Flip bottom> */}
+            <nav className="nav-block">
+                <Link className="nav-link"
+                    to="/#contact"
+                >
+                    Contact
+                </Link>
+                <Link className="nav-link"
+                    to="/#about"
+                >
+                    About
+                </Link>
+                <Link className="nav-link"
+                    to="/#projects"
+                >
+                    Projects
+                </Link>
+                <Link className="nav-link"
+                    to="/"
+                >
+                    Home
+                </Link>
+            </nav>
+        {/* </Flip> */}
+    </div>
 )
 
 export default Header
