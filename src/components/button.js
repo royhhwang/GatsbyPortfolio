6add2bda46a11ccaaab5ae50effd12c3ea01ca21
Roy Hwang <royhhwang@gmail.com>
2018-08-10 20:18:53 -0700

    button fixed

--- src/components/button.js
+++ src/components/button.js
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../css/button.css'
 import TopArrow from '../media/top-arrow.svg'
-import {Fade} from 'react-reveal'
+import { Fade } from 'react-reveal'
 
-const TopButton = () => (
-    <Fade bottom>
-        <a href="#" className="top-button">
-            <img src={TopArrow} alt="all the way up" />
-        </a>
-    </Fade>
-)
+class TopButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            height: false,
+        }
+        this.handleScreen = this.handleScreen.bind(this);
+    }
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScreen);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScreen);
+    }
+
+    handleScreen() {
+        const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+        const body = document.body;
+        const html = document.documentElement;
+        const docHeight = Math.min(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+        const docScreen = docHeight * (7 / 5);
+        const windowBottom = windowHeight + window.pageYOffset;
+        if (windowBottom >= docScreen) {
+            this.setState({ height: true });
+        }
+        else {
+            this.setState({ height: false });
+        }
+    }
+
+    render() {
+
+        let handleArrow = this.state.height ? "show-button" : "hide-button";
+
+        return (
+            <a href="#" className={handleArrow + " top-button"}>
+                <img src={TopArrow} alt="all the way up" />
+            </a>
+        )
+    }
+}
+
 
 export default TopButton
